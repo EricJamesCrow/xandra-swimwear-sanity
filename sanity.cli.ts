b98af74c39e596f5a713330dff355741e1ed64ac
Eric J Crow <EricCrow@pm.me>
2024-06-07 15:25:44 -0700

    initial commit

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'd8b2ohy4',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,64 @@
+import {defineConfig, isDev} from 'sanity'
+import {presentationTool} from 'sanity/presentation'
+
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
+
+import {visionTool} from '@sanity/vision'
+import {colorInput} from '@sanity/color-input'
+import {imageHotspotArrayPlugin} from 'sanity-plugin-hotspot-array'
+import {media, mediaAssetSource} from 'sanity-plugin-media'
+import {customDocumentActions} from './plugins/customDocumentActions'
+import Navbar from './components/studio/Navbar'
+
+const devOnlyPlugins = [visionTool()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'Shopify - xandra-swimwear',
+
+  projectId: 'd8b2ohy4',
+  dataset: 'production',
+
+  plugins: [
+    presentationTool({
+      previewUrl: {
+        previewMode: {
+          enable: isDev
+            ? 'http://localhost:3000/resource/preview'
+            : 'https://xandraswimwear.com/resource/preview',
+        },
+      },
+    }),
+    structureTool({structure}),
+    colorInput(),
+    imageHotspotArrayPlugin(),
+    customDocumentActions(),
+    media(),
+    ...(isDev ? devOnlyPlugins : []),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  form: {
+    file: {
+      assetSources: (previousAssetSources) => {
+        return previousAssetSources.filter((assetSource) => assetSource !== mediaAssetSource)
+      },
+    },
+    image: {
+      assetSources: (previousAssetSources) => {
+        return previousAssetSources.filter((assetSource) => assetSource === mediaAssetSource)
+      },
+    },
+  },
+
+  studio: {
+    components: {
+      navbar: Navbar,
+    },
+  },
+})
