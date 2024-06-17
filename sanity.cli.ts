4e00723136e598fec03fa932a786ca637b84edcb
Eric J Crow <EricCrow@pm.me>
2024-06-17 11:24:23 -0700

    fix: deploys to sanity studio now

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,15 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'd8b2ohy4',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  vite: (config) => {
+    return {
+      ...config,
+      build: {
+        ...config.build,
+        target: 'esnext',
+      },
+    }
+  },
 })
